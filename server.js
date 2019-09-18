6e7fc2f43654f9e7411f19614e33b454f45f5d85
dormin <williamsben91@gmail.com>
2019-09-17 18:29:57 -0600

    work

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ var PORT = process.env.PORT || 8080;
 // For serving of static CSS
 app.use(express.static(__dirname + "/app/css"));
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json());
+//  app.use(bodyParser.text());
+//  app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // API and HTML routes
-// require("./app/routing/apiRoutes.js")(app);
-// require("./app/routing/htmlRoutes.js")(app);
-require("./routing/apiRoutes.js")(app);
-require("./routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+ require("./app/routing/htmlRoutes.js")(app);
+// require("./routing/apiRoutes.js")(app);
+// require("./routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
